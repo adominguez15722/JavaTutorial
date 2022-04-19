7c08ed6970f0ff11fcf6285ade150a30c4dbb9aa
Anthony Dominguez <adominguez15722@gmail.com>
2022-04-19 15:57:15 -0500

    initial commit

---
+++ src/com/tutorial.java
@@ -0,0 +1,19 @@
+package com;
+
+public class tutorial {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+    }
+}
+
+// class -Contains everything - has members
+//methods - do something
+//arguments - what you pass to a method (part of the calling)
+//parameter - variables to store arguments (part of defintion)
+//statements - telling the computer something
+//properties - store something
+//
+//access modifier (public) - who can use?
+//static - no instance of class is needed
+//object - instance of a class
